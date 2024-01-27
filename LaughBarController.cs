3e68e7255158d39d07c1b34639e852da22378c05
Daniel Allan <daniel.allan@wurth.com.au>
2024-01-28 01:09:05 +1100

    few image updates to improve.

--- LaughBarController.cs
+++ LaughBarController.cs
@@ -9,9 +9,6 @@ public class LaughBarController : MonoBehaviour
 
     public Slider laughBar;
     public const int MAX_LAUGHTER = 100;
-    private float timeScale = 0;
-    private float targetLaugh;
-    private bool lerpingMood = false;
 
     // Start is called before the first frame update
     void Start()
@@ -30,27 +27,6 @@ public class LaughBarController : MonoBehaviour
     public void setMood(float mood)
     {
         laughBar.value += mood;
-        //targetLaugh = targetLaugh+mood;
-
-        //if (!lerpingMood)
-        //    StartCoroutine(LerpMood);
     }
 
-    /*
-    private IEnumerator LerpMood
-    {
-
-        float speed = 1f;
-        float startLaugh = laughBar.value;
-
-        lerpingMood = true;
-
-        while (timeScale < 1)
-        {
-            timeScale += Time.deltaTime * speed;
-            laughBar.value = Mathf.Lerp(startLaugh, targetLaugh, timeScale);
-        }
-        lerpingMood = false;
-        
-    }*/
 }
